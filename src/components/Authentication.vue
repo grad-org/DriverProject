2c7f945bb9ea5afb381e268ae7d50e1674bcb89e
Frankie <1426203851@qq.com>
2018-04-24 04:44:26 +0800

    车主认证页面

---
+++ src/components/Authentication.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="authentication">
+		<h1>车主认证</h1>
+		<mu-raised-button label="退出登录" @click="logout"/>
+	</div>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+
+			}
+		},
+		methods: {
+			logout () {
+				this.$store.dispatch('logout');
+				this.$router.push({name: 'Login'});
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.authentication {
+		height: 100px;
+	}
+</style>
