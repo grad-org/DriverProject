4e7813e962b534414a42ca9a7a6a95d0b767e92a
Frankie <1426203851@qq.com>
2018-05-01 15:09:34 +0800

    接单后的页面

---
+++ src/components/trip/HandleTrip.vue
@@ -0,0 +1,37 @@
+<template>
+	<div style="text-align: center">
+		<h1>司机接受订单</h1>
+		<mu-raised-button label="返回首页" primary @click="goHome"></mu-raised-button>
+	</div>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+
+			}
+		},
+		created () {
+			let ls_acceptedtrip = window.localStorage.getItem('AcceptedTrip');
+			if (ls_acceptedtrip == null) {
+				this.$router.push({name: 'Home'})
+			}
+		},
+		mounted () {
+			
+		},
+		methods: {
+			goHome () {
+				this.$router.push({name: 'Home'})
+			}
+		},
+		computed: {
+
+		}
+	}
+</script>
+
+<style scoped>
+	
+</style>
