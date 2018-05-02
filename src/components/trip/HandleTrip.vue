f0fbd0b5fbcccae1910fa275a8da9da47a2b47f6
Frankie <1426203851@qq.com>
2018-05-03 02:59:55 +0800

    司机接单，处理订单

--- src/components/trip/HandleTrip.vue
+++ src/components/trip/HandleTrip.vue
@@ -6,15 +6,22 @@
 </template>
 
 <script>
+
+	// import SockJS from '../../../static/utils/sockjs.js'
+	// import Stomp from 'stompjs'
+
 	export default {
 		data () {
 			return {
-
+				// 订单信息
+				ls_acceptedtrip: null,
+				// 建立连接用
+				// stompClient: Stomp.over(new SockJS('http://forcar.vip:8080/orh')),
 			}
 		},
 		created () {
-			let ls_acceptedtrip = window.localStorage.getItem('AcceptedTrip');
-			if (ls_acceptedtrip == null) {
+			this.ls_acceptedtrip = window.localStorage.getItem('AcceptedTrip');
+			if (this.ls_acceptedtrip == null) {
 				this.$router.push({name: 'Home'})
 			}
 		},
@@ -24,7 +31,27 @@
 		methods: {
 			goHome () {
 				this.$router.push({name: 'Home'})
-			}
+				window.localStorage.removeItem('AcceptTrip');
+			},
+			// 发送位置到对应乘客，后续需要完成的是，隔N秒发送一次位置
+			// sendLocation () {
+			// 	let _this = this;
+			// 	let token = window.localStorage.getItem('Token')
+			// 	// 创建连接
+			// 	_this.stompClient.connect(
+			// 		// headers
+			// 		{'Auth-Token': token},
+			// 		// 连接成功的回调函数
+			// 		function connectCallback (frame) {
+			// 			_this.stompClient.send('/api/queue/hailingService/car/uploadCarLocation/')
+			// 		},
+			// 		// 连接失败的回调函数
+			// 		function errorCallback (error) {
+			// 			console.log(error);
+			// 			console.log('失败回调',error);
+			// 		}
+			// 	)
+			// },
 		},
 		computed: {
 
