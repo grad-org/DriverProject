8f5775391e03be69db74bca4d6f9965163b96056
Frankie <1426203851@qq.com>
2018-05-05 02:34:37 +0800

    自适应屏幕高度

--- src/components/trip/HandleTrip.vue
+++ src/components/trip/HandleTrip.vue
@@ -1,38 +1,52 @@
 <template>
-	<div style="text-align: center">
-		<h1>司机接受订单</h1>
-		<mu-raised-button label="返回首页" primary @click="goHome"></mu-raised-button>
+	<div>
+		<mu-appbar ref="barDiv" :title="passengerLocationTitle" style="text-align: center" />
+		<baidu-map-go ref="mapDiv" :style="{height: mapHeight}"></baidu-map-go>
+		<selector-go ref="selectDiv"></selector-go>
 	</div>
 </template>
 
 <script>
 
-	// import SockJS from '../../../static/utils/sockjs.js'
-	// import Stomp from 'stompjs'
+	import BaiduMapGo from '../map/BaiduMapGo'
+	import SelectorGo from '../selector/SelectorGo'
+	import SockJS from '../../../static/utils/sockjs.js'
+	import Stomp from 'stompjs'
 
 	export default {
+		components: {
+			BaiduMapGo, SelectorGo
+		},
 		data () {
 			return {
-				// 订单信息
-				ls_acceptedtrip: null,
-				// 建立连接用
-				// stompClient: Stomp.over(new SockJS('http://forcar.vip:8080/orh')),
+				passengerLocationTitle: '前往接乘客...',
+
+				// 首页组件高度控制
+				fullHeight: document.documentElement.clientHeight,
+				barHeight: '',
+				selectHeight: '',
+				mapHeight: ''
 			}
 		},
 		created () {
-			this.ls_acceptedtrip = window.localStorage.getItem('AcceptedTrip');
-			if (this.ls_acceptedtrip == null) {
-				this.$router.push({name: 'Home'})
+			let ls_acceptedtrip = JSON.parse(window.localStorage.getItem('AcceptedTrip'));
+			if (ls_acceptedtrip == null) {
+				this.$router.push({name: 'Home'});
+			} else if (ls_acceptedtrip.departure == '当前位置') {
+				// 这种情况是，乘客发布行程时，起点无法识别具体一个位置名称
+				this.passengerLocationTitle = '前往接乘客...'
 			}
+			else {
+				this.passengerLocationTitle = '前往' + ls_acceptedtrip.departure + '接乘客...';
+			};
+			this.initHeight();
+			this.setMapHeight();
 		},
 		mounted () {
 			
 		},
 		methods: {
-			goHome () {
-				this.$router.push({name: 'Home'})
-				window.localStorage.removeItem('AcceptTrip');
-			},
+
 			// 发送位置到对应乘客，后续需要完成的是，隔N秒发送一次位置
 			// sendLocation () {
 			// 	let _this = this;
@@ -52,9 +66,45 @@
 			// 		}
 			// 	)
 			// },
+			initHeight () {
+				let _this = this
+				window.onresize = function () {
+					return ( ()=> {
+						// 浏览器内容可视高度
+						window.fullHeight = document.documentElement.clientHeight;
+						_this.fullHeight = window.fullHeight;
+						_this.selectHeight = _this.$refs.selectDiv.$el.clientHeight;
+						_this.mapHeight = (_this.fullHeight - _this.barHeight - _this.selectHeight) + 'px';
+					}) ()
+				}
+			},
+			setMapHeight () {
+				this.$nextTick (() => {
+					this.barHeight = this.$refs.barDiv.$el.clientHeight;
+					this.selectHeight = this.$refs.selectDiv.$el.clientHeight;
+					// 页面加载后，对地图高度进行设置
+					let h = this.fullHeight - this.barHeight - this.selectHeight;
+					// console.log('地图高度：' +h);
+					this.mapHeight = h + 'px';
+				})
+			},
 		},
 		computed: {
 
+		},
+		watch: {
+			// 如果 fullHeight 发生改变，这个函数就会运行
+			fullHeight (val) {
+				if(!this.timer) {
+					this.fullHeight = val
+					this.timer = true
+					let that = this
+					setTimeout(function () {
+						that.timer = false
+					}, 1000)
+				}
+				// console.log("触发watch的fullHeight")
+			}
 		}
 	}
 </script>
