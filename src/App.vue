54ceb62d9b65b9313d9e1bccb533e83afddeca03
Frankie <1426203851@qq.com>
2018-05-23 01:49:11 +0800

    打印WebSocket连接成功信息

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,14 @@
 </template>
 
 <script>
+	import SocketIO from '../static/utils/socket.io.js'
 	export default {
-		name: 'App'
+		name: 'App',
+		sockets: {
+			connect: function () {
+				console.log('WebSocket连接成功了，ID是：', this.$socket.id);
+			}
+		}
 	}
 </script>
 
